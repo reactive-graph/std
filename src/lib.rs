036ab1c76fad47d1865f898d87f242f0a5f1717e
Andreas Schaeffer <andreas@schaeffernet.de>
2021-09-08 21:48:30 +0200

    Added plugin construction for arithmetic gates

---
+++ src/lib.rs
@@ -0,0 +1,66 @@
+#![feature(register_tool)]
+#![register_tool(tarpaulin)]
+
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate query_interface;
+
+use std::sync::Arc;
+
+use inexor_rgf_core_frp as frp;
+use inexor_rgf_core_model as model;
+use inexor_rgf_core_plugins as plugins;
+use inexor_rgf_core_reactive as reactive;
+use log::error;
+use waiter_di::{profiles, Container, Provider};
+
+use crate::plugin::ArithmeticPlugin;
+use crate::plugins::{Plugin, PluginError};
+
+pub mod behaviour;
+pub mod plugin;
+pub mod provider;
+
+pub fn get<T>() -> Container<T> {
+    Container::<T>::new()
+}
+
+pub fn construct_plugin() -> Result<Arc<dyn Plugin>, PluginError> {
+    let mut container = get::<profiles::Default>();
+    let container = &mut container;
+    let plugin = Provider::<dyn ArithmeticPlugin>::create(container);
+    let plugin = Arc::new(plugin);
+    let plugin: Result<Arc<dyn Plugin>, _> =
+        <dyn query_interface::Object>::query_arc(plugin.clone());
+    if plugin.is_err() {
+        error!("Failed to construct plugin");
+        return Err(PluginError::PluginCreationError);
+    }
+    Ok(plugin.unwrap())
+}
+
+plugins::export_plugin!(register);
+
+extern "C" fn register(registrar: &mut dyn plugins::PluginRegistrar) {
+    let logger_result = log4rs::init_file("config/logging.yml", Default::default());
+    match logger_result {
+        Err(error) => {
+            println!("Failed to configure logger: {}", error);
+        }
+        _ => {}
+    }
+
+    let plugin = construct_plugin();
+    match plugin {
+        Ok(plugin) => {
+            const PKG_NAME: &'static str = env!("CARGO_PKG_NAME");
+            registrar.register_plugin(PKG_NAME, Box::new(plugin));
+        }
+        Err(_) => {}
+    }
+}
+
+#[cfg(test)]
+#[tarpaulin::ignore]
+pub mod tests;
---
+++ src/plugin.rs
@@ -0,0 +1,96 @@
+use std::sync::Arc;
+
+use async_trait::async_trait;
+use log::debug;
+use waiter_di::*;
+
+use crate::behaviour::entity::entity_behaviour_provider::ArithmeticEntityBehaviourProviderImpl;
+use crate::plugins::{
+    ComponentProvider, EntityBehaviourProvider, EntityTypeProvider, FlowProvider, Plugin,
+    PluginError, RelationBehaviourProvider, RelationTypeProvider,
+};
+use crate::provider::{ArithmeticComponentProviderImpl, ArithmeticEntityTypeProviderImpl};
+
+#[async_trait]
+pub trait ArithmeticPlugin: Plugin + Send + Sync {}
+
+#[module]
+pub struct ArithmeticPluginImpl {
+    component_provider: Wrc<ArithmeticComponentProviderImpl>,
+    entity_type_provider: Wrc<ArithmeticEntityTypeProviderImpl>,
+    entity_behaviour_provider: Wrc<ArithmeticEntityBehaviourProviderImpl>,
+}
+
+interfaces!(ArithmeticPluginImpl: dyn Plugin);
+
+#[async_trait]
+#[provides]
+impl ArithmeticPlugin for ArithmeticPluginImpl {}
+
+impl Plugin for ArithmeticPluginImpl {
+    fn init(&self) -> Result<(), PluginError> {
+        debug!("ArithmeticPluginModuleImpl::init()");
+        Ok(())
+    }
+
+    fn post_init(&self) -> Result<(), PluginError> {
+        debug!("ArithmeticPluginModuleImpl::post_init()");
+        Ok(())
+    }
+
+    fn pre_shutdown(&self) -> Result<(), PluginError> {
+        debug!("ArithmeticPluginModuleImpl::pre_shutdown()");
+        Ok(())
+    }
+
+    fn shutdown(&self) -> Result<(), PluginError> {
+        debug!("ArithmeticPluginModuleImpl::shutdown()");
+        Ok(())
+    }
+
+    fn get_component_provider(&self) -> Result<Arc<dyn ComponentProvider>, PluginError> {
+        let component_provider = self.component_provider.clone();
+        let component_provider: Result<Arc<dyn ComponentProvider>, _> =
+            <dyn query_interface::Object>::query_arc(component_provider);
+        if component_provider.is_err() {
+            return Err(PluginError::NoComponentProvider);
+        }
+        Ok(component_provider.unwrap())
+    }
+
+    fn get_entity_type_provider(&self) -> Result<Arc<dyn EntityTypeProvider>, PluginError> {
+        let entity_type_provider = self.entity_type_provider.clone();
+        let entity_type_provider: Result<Arc<dyn EntityTypeProvider>, _> =
+            <dyn query_interface::Object>::query_arc(entity_type_provider);
+        if entity_type_provider.is_err() {
+            return Err(PluginError::NoEntityTypeProvider);
+        }
+        Ok(entity_type_provider.unwrap())
+    }
+
+    fn get_relation_type_provider(&self) -> Result<Arc<dyn RelationTypeProvider>, PluginError> {
+        Err(PluginError::NoRelationTypeProvider)
+    }
+
+    fn get_entity_behaviour_provider(
+        &self,
+    ) -> Result<Arc<dyn EntityBehaviourProvider>, PluginError> {
+        let entity_behaviour_provider = self.entity_behaviour_provider.clone();
+        let entity_behaviour_provider: Result<Arc<dyn EntityBehaviourProvider>, _> =
+            <dyn query_interface::Object>::query_arc(entity_behaviour_provider);
+        if entity_behaviour_provider.is_err() {
+            return Err(PluginError::NoEntityBehaviourProvider);
+        }
+        Ok(entity_behaviour_provider.unwrap())
+    }
+
+    fn get_relation_behaviour_provider(
+        &self,
+    ) -> Result<Arc<dyn RelationBehaviourProvider>, PluginError> {
+        Err(PluginError::NoRelationBehaviourProvider)
+    }
+
+    fn get_flow_provider(&self) -> Result<Arc<dyn FlowProvider>, PluginError> {
+        Err(PluginError::NoFlowProvider)
+    }
+}
