2d0f84363394bb580eb376d83ec44c2aa76c563f
Andreas Schaeffer <andreas@schaeffernet.de>
2021-09-08 19:49:01 +0200

    Sort alphabetically

--- src/behaviour/entity/gate/function.rs
+++ src/behaviour/entity/gate/function.rs
@@ -4,18 +4,17 @@ use std::collections::HashMap;
 // TODO: Different types for input and output (e.g. abs)
 pub type NumericGateFunction<T> = fn(T, T) -> T;
 
-// TODO: Integer
-pub const FN_POW_F64: NumericGateFunction<f64> = |lhs, rhs| lhs.powf(rhs);
-pub const FN_LOG_F64: NumericGateFunction<f64> = |lhs, rhs| lhs.log(rhs);
-pub const FN_HYPOT_F64: NumericGateFunction<f64> = |lhs, rhs| lhs.hypot(rhs);
 pub const FN_ATAN2_F64: NumericGateFunction<f64> = |lhs, rhs| lhs.atan2(rhs);
+pub const FN_HYPOT_F64: NumericGateFunction<f64> = |lhs, rhs| lhs.hypot(rhs);
+pub const FN_LOG_F64: NumericGateFunction<f64> = |lhs, rhs| lhs.log(rhs);
+pub const FN_POW_F64: NumericGateFunction<f64> = |lhs, rhs| lhs.powf(rhs);
 
 lazy_static! {
     pub static ref NUMERIC_GATES: HashMap<&'static str, NumericGateFunction<f64>> = vec![
-        ("pow", FN_POW_F64),
-        ("log", FN_POW_F64),
-        ("hypot", FN_HYPOT_F64),
         ("atan2", FN_ATAN2_F64),
+        ("hypot", FN_HYPOT_F64),
+        ("log", FN_POW_F64),
+        ("pow", FN_POW_F64),
     ]
     .into_iter()
     .collect();
