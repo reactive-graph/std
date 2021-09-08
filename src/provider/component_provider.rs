f33d4e12ee4b5138179bfbb4832d49a0defb3f20
Andreas Schaeffer <andreas@schaeffernet.de>
2021-09-08 21:47:53 +0200

    Added component provider and entity type provider for arithmetic gates

---
+++ src/provider/component_provider.rs
@@ -0,0 +1,56 @@
+use async_trait::async_trait;
+use log::{debug, error};
+use rust_embed::RustEmbed;
+use waiter_di::*;
+
+use crate::model::component::Component;
+use crate::plugins::ComponentProvider;
+
+#[derive(RustEmbed)]
+#[folder = "./assets/types/components"]
+struct ArithmeticComponentAsset;
+
+#[async_trait]
+pub trait ArithmeticComponentProvider: ComponentProvider + Send + Sync {}
+
+#[derive(Clone)]
+pub struct ArithmeticComponentProviderImpl {}
+
+interfaces!(ArithmeticComponentProviderImpl: dyn ComponentProvider);
+
+#[component]
+impl ArithmeticComponentProviderImpl {
+    #[provides]
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+#[async_trait]
+#[provides]
+impl ArithmeticComponentProvider for ArithmeticComponentProviderImpl {}
+
+impl ComponentProvider for ArithmeticComponentProviderImpl {
+    fn get_components(&self) -> Vec<Component> {
+        let mut components = Vec::new();
+        for file in ArithmeticComponentAsset::iter() {
+            let filename = file.as_ref();
+            debug!("Loading component from resource {}", filename);
+            let asset = ArithmeticComponentAsset::get(filename).unwrap();
+            let json_str = std::str::from_utf8(asset.data.as_ref());
+            if json_str.is_err() {
+                error!("Could not decode UTF-8 {}", filename);
+                continue;
+            }
+            let component: Component = match serde_json::from_str(json_str.unwrap()) {
+                Result::Ok(component) => component,
+                Result::Err(err) => {
+                    error!("Error in parsing JSON file {}: {}", filename, err);
+                    continue;
+                }
+            };
+            components.push(component);
+        }
+        components
+    }
+}
