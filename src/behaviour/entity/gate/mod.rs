737fec2658ce77ced03286ccb457d22f41965603
Andreas Schaeffer <andreas@schaeffernet.de>
2021-09-08 21:47:24 +0200

    Added entity behaviour provider for arithmetic gates

---
+++ src/behaviour/entity/gate/mod.rs
@@ -0,0 +1,7 @@
+pub use arithmetic_gate::ArithmeticGate;
+pub use function::ArithmeticGateFunction;
+pub use function::ARITHMETIC_GATES;
+
+pub mod arithmetic_gate;
+pub mod function;
+pub mod properties;
