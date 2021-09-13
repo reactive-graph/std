c6ed67a3e2d1fbcefa24cbb9c4498b3d6c37d71e
Andreas Schaeffer <andreas@schaeffernet.de>
2021-09-13 19:04:50 +0200

    Updated plugin trait (WebResourceProvider)

--- src/plugin.rs
+++ src/plugin.rs
@@ -7,7 +7,7 @@ use waiter_di::*;
 use crate::behaviour::entity::entity_behaviour_provider::NumericEntityBehaviourProviderImpl;
 use crate::plugins::{
     ComponentProvider, EntityBehaviourProvider, EntityTypeProvider, FlowProvider, Plugin,
-    PluginError, RelationBehaviourProvider, RelationTypeProvider,
+    PluginError, RelationBehaviourProvider, RelationTypeProvider, WebResourceProvider,
 };
 use crate::provider::{NumericComponentProviderImpl, NumericEntityTypeProviderImpl};
 
@@ -92,5 +92,9 @@ impl Plugin for NumericPluginImpl {
 
     fn get_flow_provider(&self) -> Result<Arc<dyn FlowProvider>, PluginError> {
         Err(PluginError::NoFlowProvider)
+    }
+
+    fn get_web_resource_provider(&self) -> Result<Arc<dyn WebResourceProvider>, PluginError> {
+        Err(PluginError::NoWebResourceProvider)
     }
 }
