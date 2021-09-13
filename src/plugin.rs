075f2d486c23784a07fe0d116b3b7f0495ae9f3c
Andreas Schaeffer <andreas@schaeffernet.de>
2021-09-13 22:52:58 +0200

    Updated plugin trait (PluginMetadata + ComponentBehaviourProvider)

--- src/plugin.rs
+++ src/plugin.rs
@@ -4,9 +4,11 @@ use async_trait::async_trait;
 use log::debug;
 use waiter_di::*;
 
+use crate::plugins::plugin::PluginMetadata;
 use crate::plugins::{
-    ComponentProvider, EntityBehaviourProvider, EntityTypeProvider, FlowProvider, Plugin,
-    PluginError, RelationBehaviourProvider, RelationTypeProvider, WebResourceProvider,
+    ComponentBehaviourProvider, ComponentProvider, EntityBehaviourProvider, EntityTypeProvider,
+    FlowProvider, Plugin, PluginError, RelationBehaviourProvider, RelationTypeProvider,
+    WebResourceProvider,
 };
 use crate::provider::MetaDataComponentProviderImpl;
 
@@ -25,6 +27,14 @@ interfaces!(MetaDataPluginImpl: dyn Plugin);
 impl MetaDataPlugin for MetaDataPluginImpl {}
 
 impl Plugin for MetaDataPluginImpl {
+    fn metadata(&self) -> Result<PluginMetadata, PluginError> {
+        Ok(PluginMetadata {
+            name: env!("CARGO_PKG_NAME").into(),
+            description: env!("CARGO_PKG_DESCRIPTION").into(),
+            version: env!("CARGO_PKG_VERSION").into(),
+        })
+    }
+
     fn init(&self) -> Result<(), PluginError> {
         debug!("MetaDataPluginModuleImpl::init()");
         Ok(())
@@ -61,6 +71,12 @@ impl Plugin for MetaDataPluginImpl {
 
     fn get_relation_type_provider(&self) -> Result<Arc<dyn RelationTypeProvider>, PluginError> {
         Err(PluginError::NoRelationTypeProvider)
+    }
+
+    fn get_component_behaviour_provider(
+        &self,
+    ) -> Result<Arc<dyn ComponentBehaviourProvider>, PluginError> {
+        Err(PluginError::NoComponentBehaviourProvider)
     }
 
     fn get_entity_behaviour_provider(
