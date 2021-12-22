1b12b3f8afc060bce093e8cc3ca151e6acee84cc
Andreas Schaeffer <andreas@schaeffernet.de>
2021-12-22 01:40:17 +0100

    Edition 2021 is now stable + accept bugfix versions + use plugin context

--- src/plugin.rs
+++ src/plugin.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::sync::{Arc, RwLock};
 
 use async_trait::async_trait;
 use log::debug;
@@ -6,12 +6,21 @@ use waiter_di::*;
 
 use crate::behaviour::entity::entity_behaviour_provider::LogicalEntityBehaviourProviderImpl;
 use crate::plugins::plugin::PluginMetadata;
+use crate::plugins::plugin_context::PluginContext;
 use crate::plugins::{
     ComponentBehaviourProvider, ComponentProvider, EntityBehaviourProvider, EntityTypeProvider,
     FlowProvider, Plugin, PluginError, RelationBehaviourProvider, RelationTypeProvider,
     WebResourceProvider,
 };
 use crate::provider::{LogicalComponentProviderImpl, LogicalEntityTypeProviderImpl};
+
+#[wrapper]
+pub struct PluginContextContainer(RwLock<Option<std::sync::Arc<dyn PluginContext>>>);
+
+#[provides]
+fn create_empty_plugin_context_container() -> PluginContextContainer {
+    return PluginContextContainer(RwLock::new(None));
+}
 
 #[async_trait]
 pub trait LogicalPlugin: Plugin + Send + Sync {}
@@ -21,6 +30,8 @@ pub struct LogicalPluginImpl {
     component_provider: Wrc<LogicalComponentProviderImpl>,
     entity_type_provider: Wrc<LogicalEntityTypeProviderImpl>,
     entity_behaviour_provider: Wrc<LogicalEntityBehaviourProviderImpl>,
+
+    context: PluginContextContainer,
 }
 
 interfaces!(LogicalPluginImpl: dyn Plugin);
@@ -55,6 +66,11 @@ impl Plugin for LogicalPluginImpl {
 
     fn shutdown(&self) -> Result<(), PluginError> {
         debug!("LogicalPluginModuleImpl::shutdown()");
+        Ok(())
+    }
+
+    fn set_context(&self, context: Arc<dyn PluginContext>) -> Result<(), PluginError> {
+        self.context.0.write().unwrap().replace(context);
         Ok(())
     }
 
