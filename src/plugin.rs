8feb02b97def743e791bf740ca860f8e18bbdaab
Andreas Schaeffer <andreas@schaeffernet.de>
2022-02-05 22:09:04 +0100

    Refactor di

--- src/plugin.rs
+++ src/plugin.rs
@@ -1,8 +1,8 @@
 use std::sync::{Arc, RwLock};
 
+use crate::di::*;
 use async_trait::async_trait;
 use log::{debug, error};
-use waiter_di::*;
 
 use crate::plugins::plugin::PluginMetadata;
 use crate::plugins::plugin_context::PluginContext;
