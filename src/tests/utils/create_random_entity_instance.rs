160fad79dc8e6e836f289b8990adfb893844adbe
Andreas Schaeffer <andreas@schaeffernet.de>
2022-02-27 17:21:43 +0100

    Fix unit tests to reflect the upgrade to indradb v3

--- src/tests/utils/create_random_entity_instance.rs
+++ src/tests/utils/create_random_entity_instance.rs
@@ -1,6 +1,6 @@
 use std::str::FromStr;
 
-use indradb::{NamedProperty, Type, Vertex, VertexProperties};
+use indradb::{Identifier, NamedProperty, Vertex, VertexProperties};
 use serde_json::json;
 use uuid::Uuid;
 
@@ -10,11 +10,11 @@ use crate::tests::utils::r_string;
 pub fn create_random_entity_instance<S: Into<String>>(property_name: S) -> ReactiveEntityInstance {
     let uuid = Uuid::new_v4();
     let type_name = r_string();
-    let t = Type::from_str(type_name.as_str()).unwrap();
+    let t = Identifier::from_str(type_name.as_str()).unwrap();
     let property_value = r_string();
     let property_value_json = json!(property_value);
     let property = NamedProperty {
-        name: property_name.into().clone(),
+        name: Identifier::from_str(property_name.into().as_str()).unwrap(),
         value: property_value_json,
     };
     let properties = vec![property];
