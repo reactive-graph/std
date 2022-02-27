a8142b475bcf1a5f3e6b462fecbd78ba006f8d21
Andreas Schaeffer <andreas@schaeffernet.de>
2022-02-27 17:22:33 +0100

    Cleanup unused code

--- src/tests/utils/mod.rs
+++ src/tests/utils/mod.rs
@@ -1,5 +1,4 @@
 use random_string::generate;
-use serde_json::{json, Value};
 
 pub use create_connector::*;
 pub use create_connector_relation_instance_with_properties::*;
@@ -13,16 +12,4 @@ const CHARSET_LETTERS: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVW
 
 pub fn r_string() -> String {
     generate(10, CHARSET_LETTERS).to_string()
-}
-
-pub fn r_string_255() -> String {
-    generate(255, CHARSET_LETTERS).to_string()
-}
-
-pub fn r_string_1000() -> String {
-    generate(1000, CHARSET_LETTERS).to_string()
-}
-
-pub fn r_json_string() -> Value {
-    json!(r_string())
 }
