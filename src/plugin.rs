ae34b64756f7357cb8e5af083936e7b17c29aa07
Andreas Schaeffer <andreas@schaeffernet.de>
2022-09-25 02:19:46 +0200

    Upgrade to plugin api 0.7.0

--- src/plugin.rs
+++ src/plugin.rs
@@ -1,16 +1,18 @@
-use std::sync::{Arc, RwLock};
+use std::sync::Arc;
+use std::sync::RwLock;
+
+use async_trait::async_trait;
 
 use crate::di::*;
-use async_trait::async_trait;
-use log::debug;
-
+use crate::plugins::component_provider;
 use crate::plugins::plugin::PluginMetadata;
+use crate::plugins::plugin::PluginMetadataError;
 use crate::plugins::plugin_context::PluginContext;
-use crate::plugins::{
-    ComponentBehaviourProvider, ComponentProvider, EntityBehaviourProvider, EntityTypeProvider,
-    FlowProvider, Plugin, PluginError, RelationBehaviourProvider, RelationTypeProvider,
-    WebResourceProvider,
-};
+use crate::plugins::plugin_metadata;
+use crate::plugins::ComponentProvider;
+use crate::plugins::ComponentProviderError;
+use crate::plugins::Plugin;
+use crate::plugins::PluginContextInitializationError;
 use crate::provider::MetaDataComponentProviderImpl;
 
 #[wrapper]
@@ -38,80 +40,21 @@ interfaces!(MetaDataPluginImpl: dyn Plugin);
 impl MetaDataPlugin for MetaDataPluginImpl {}
 
 impl Plugin for MetaDataPluginImpl {
-    fn metadata(&self) -> Result<PluginMetadata, PluginError> {
-        Ok(PluginMetadata {
-            name: env!("CARGO_PKG_NAME").into(),
-            description: env!("CARGO_PKG_DESCRIPTION").into(),
-            version: env!("CARGO_PKG_VERSION").into(),
-        })
+    fn metadata(&self) -> Result<PluginMetadata, PluginMetadataError> {
+        plugin_metadata!("inexor-rgf-plugin-base")
     }
 
-    fn init(&self) -> Result<(), PluginError> {
-        debug!("MetaDataPluginModuleImpl::init()");
-        Ok(())
-    }
-
-    fn post_init(&self) -> Result<(), PluginError> {
-        debug!("MetaDataPluginModuleImpl::post_init()");
-        Ok(())
-    }
-
-    fn pre_shutdown(&self) -> Result<(), PluginError> {
-        debug!("MetaDataPluginModuleImpl::shutdown()");
-        Ok(())
-    }
-
-    fn shutdown(&self) -> Result<(), PluginError> {
-        debug!("MetaDataPluginModuleImpl::shutdown()");
-        Ok(())
-    }
-
-    fn set_context(&self, context: Arc<dyn PluginContext>) -> Result<(), PluginError> {
+    fn set_context(
+        &self,
+        context: Arc<dyn PluginContext>,
+    ) -> Result<(), PluginContextInitializationError> {
         self.context.0.write().unwrap().replace(context);
         Ok(())
     }
 
-    fn get_component_provider(&self) -> Result<Arc<dyn ComponentProvider>, PluginError> {
-        let component_provider = self.component_provider.clone();
-        let component_provider: Result<Arc<dyn ComponentProvider>, _> =
-            <dyn query_interface::Object>::query_arc(component_provider);
-        if component_provider.is_err() {
-            return Err(PluginError::NoComponentProvider);
-        }
-        Ok(component_provider.unwrap())
-    }
-
-    fn get_entity_type_provider(&self) -> Result<Arc<dyn EntityTypeProvider>, PluginError> {
-        Err(PluginError::NoEntityTypeProvider)
-    }
-
-    fn get_relation_type_provider(&self) -> Result<Arc<dyn RelationTypeProvider>, PluginError> {
-        Err(PluginError::NoRelationTypeProvider)
-    }
-
-    fn get_component_behaviour_provider(
+    fn get_component_provider(
         &self,
-    ) -> Result<Arc<dyn ComponentBehaviourProvider>, PluginError> {
-        Err(PluginError::NoComponentBehaviourProvider)
-    }
-
-    fn get_entity_behaviour_provider(
-        &self,
-    ) -> Result<Arc<dyn EntityBehaviourProvider>, PluginError> {
-        Err(PluginError::NoEntityBehaviourProvider)
-    }
-
-    fn get_relation_behaviour_provider(
-        &self,
-    ) -> Result<Arc<dyn RelationBehaviourProvider>, PluginError> {
-        Err(PluginError::NoRelationBehaviourProvider)
-    }
-
-    fn get_flow_provider(&self) -> Result<Arc<dyn FlowProvider>, PluginError> {
-        Err(PluginError::NoFlowProvider)
-    }
-
-    fn get_web_resource_provider(&self) -> Result<Arc<dyn WebResourceProvider>, PluginError> {
-        Err(PluginError::NoWebResourceProvider)
+    ) -> Result<Option<Arc<dyn ComponentProvider>>, ComponentProviderError> {
+        component_provider!(self.component_provider)
     }
 }
