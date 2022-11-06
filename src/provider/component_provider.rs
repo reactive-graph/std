5676ee50651e00196e40984559cfc572691d6e4b
Andreas Schaeffer <andreas@schaeffernet.de>
2022-11-06 12:53:34 +0100

    Replace provider implementations with macro

--- src/provider/component_provider.rs
+++ src/provider/component_provider.rs
@@ -5,6 +5,7 @@ use rust_embed::RustEmbed;
 
 use crate::di::*;
 use crate::model::Component;
+use crate::plugins::embedded_asset_provider_impl;
 use crate::plugins::ComponentProvider;
 
 #[derive(RustEmbed)]
@@ -33,25 +34,6 @@ impl ValueComponentProvider for ValueComponentProviderImpl {}
 
 impl ComponentProvider for ValueComponentProviderImpl {
     fn get_components(&self) -> Vec<Component> {
-        let mut components = Vec::new();
-        for file in ValueComponentAsset::iter() {
-            let filename = file.as_ref();
-            debug!("Loading component from resource {}", filename);
-            let asset = ValueComponentAsset::get(filename).unwrap();
-            let json_str = std::str::from_utf8(asset.data.as_ref());
-            if json_str.is_err() {
-                error!("Could not decode UTF-8 {}", filename);
-                continue;
-            }
-            let component: Component = match serde_json::from_str(json_str.unwrap()) {
-                Result::Ok(component) => component,
-                Result::Err(err) => {
-                    error!("Error in parsing JSON file {}: {}", filename, err);
-                    continue;
-                }
-            };
-            components.push(component);
-        }
-        components
+        embedded_asset_provider_impl!(ValueComponentAsset, Component)
     }
 }
