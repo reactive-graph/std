f0cd7323922b3edb988646f08283960f0010304d
Andreas Schaeffer <andreas@schaeffernet.de>
2022-11-06 20:08:35 +0100

    Use macro for type providers

--- src/provider/component_provider.rs
+++ src/provider/component_provider.rs
@@ -1,9 +1,11 @@
-use crate::di::*;
 use async_trait::async_trait;
-use log::{debug, error};
+use log::debug;
+use log::error;
 use rust_embed::RustEmbed;
 
+use crate::di::*;
 use crate::model::component::Component;
+use crate::plugins::embedded_asset_provider_impl;
 use crate::plugins::ComponentProvider;
 
 #[derive(RustEmbed)]
@@ -32,25 +34,6 @@ impl TaxonomyComponentProvider for TaxonomyComponentProviderImpl {}
 
 impl ComponentProvider for TaxonomyComponentProviderImpl {
     fn get_components(&self) -> Vec<Component> {
-        let mut components = Vec::new();
-        for file in TaxonomyComponentAsset::iter() {
-            let filename = file.as_ref();
-            debug!("Loading component from resource {}", filename);
-            let asset = TaxonomyComponentAsset::get(filename).unwrap();
-            let json_str = std::str::from_utf8(asset.data.as_ref());
-            if json_str.is_err() {
-                error!("Could not decode UTF-8 {}", filename);
-                continue;
-            }
-            let component: Component = match serde_json::from_str(json_str.unwrap()) {
-                Result::Ok(component) => component,
-                Result::Err(err) => {
-                    error!("Error in parsing JSON file {}: {}", filename, err);
-                    continue;
-                }
-            };
-            components.push(component);
-        }
-        components
+        embedded_asset_provider_impl!(TaxonomyComponentAsset, Component)
     }
 }
