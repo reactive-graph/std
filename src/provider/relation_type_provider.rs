907bcedd24b9fb90b98d4975667d7fc87fd0f359
Andreas Schaeffer <andreas@schaeffernet.de>
2022-11-06 20:10:00 +0100

    Use macro for type providers

--- src/provider/relation_type_provider.rs
+++ src/provider/relation_type_provider.rs
@@ -1,9 +1,11 @@
-use crate::di::*;
 use async_trait::async_trait;
-use log::{debug, error};
+use log::debug;
+use log::error;
 use rust_embed::RustEmbed;
 
+use crate::di::*;
 use crate::model::RelationType;
+use crate::plugins::embedded_asset_provider_impl;
 use crate::plugins::RelationTypeProvider;
 
 #[derive(RustEmbed)]
@@ -32,25 +34,6 @@ impl TaxonomyRelationTypeProvider for TaxonomyRelationTypeProviderImpl {}
 
 impl RelationTypeProvider for TaxonomyRelationTypeProviderImpl {
     fn get_relation_types(&self) -> Vec<RelationType> {
-        let mut relation_types = Vec::new();
-        for file in TaxonomyRelationTypeAsset::iter() {
-            let filename = file.as_ref();
-            debug!("Loading relation_type from resource {}", filename);
-            let asset = TaxonomyRelationTypeAsset::get(filename).unwrap();
-            let json_str = std::str::from_utf8(asset.data.as_ref());
-            if json_str.is_err() {
-                error!("Could not decode UTF-8 {}", filename);
-                continue;
-            }
-            let relation_type: RelationType = match serde_json::from_str(json_str.unwrap()) {
-                Result::Ok(relation_type) => relation_type,
-                Result::Err(err) => {
-                    error!("Error in parsing JSON file {}: {}", filename, err);
-                    continue;
-                }
-            };
-            relation_types.push(relation_type);
-        }
-        relation_types
+        embedded_asset_provider_impl!(TaxonomyRelationTypeAsset, RelationType)
     }
 }
