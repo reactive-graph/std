c6538c5375e116ddb006847f8fe96fb19aa70d7b
Andreas Schaeffer <andreas@schaeffernet.de>
2022-11-18 17:28:07 +0100

    Replace duplicated similar code by macros

--- src/provider/component_provider.rs
+++ src/provider/component_provider.rs
@@ -1,39 +1,12 @@
-use async_trait::async_trait;
 use log::debug;
 use log::error;
+use paste::paste;
 use rust_embed::RustEmbed;
 
 use crate::di::*;
 use crate::model::Component;
+use crate::plugins::component_provider_impl;
 use crate::plugins::embedded_asset_provider_impl;
 use crate::plugins::ComponentProvider;
 
-#[derive(RustEmbed)]
-#[folder = "./assets/types/components"]
-struct MetaDataComponentAsset;
-
-#[async_trait]
-pub trait MetaDataComponentProvider: ComponentProvider + Send + Sync {}
-
-#[derive(Clone)]
-pub struct MetaDataComponentProviderImpl {}
-
-interfaces!(MetaDataComponentProviderImpl: dyn ComponentProvider);
-
-#[component]
-impl MetaDataComponentProviderImpl {
-    #[provides]
-    fn new() -> Self {
-        Self {}
-    }
-}
-
-#[async_trait]
-#[provides]
-impl MetaDataComponentProvider for MetaDataComponentProviderImpl {}
-
-impl ComponentProvider for MetaDataComponentProviderImpl {
-    fn get_components(&self) -> Vec<Component> {
-        embedded_asset_provider_impl!(MetaDataComponentAsset, Component)
-    }
-}
+component_provider_impl!(MetaData, "./assets/types/components");
