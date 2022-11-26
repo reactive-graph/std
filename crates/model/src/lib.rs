500d5ee0758d14283f60462704ef285a4f6e783a
Andreas Schaeffer <andreas@schaeffernet.de>
2022-11-26 20:31:41 +0100

    Use macro for defining properties

--- crates/model/src/lib.rs
+++ crates/model/src/lib.rs
@@ -3,7 +3,6 @@ pub use component::*;
 pub mod component;
 
 use inexor_rgf_core_model as model;
-use inexor_rgf_core_reactive as reactive;
 
 pub const NAMESPACE_VALUE: &str = "value";
 pub const NAMESPACE_STATE: &str = "state";
