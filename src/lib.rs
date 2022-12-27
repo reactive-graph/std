46f72e2f3d16cf7fa7ea9363d0de6d4e862d0eb8
Andreas Schaeffer <andreas@schaeffernet.de>
2022-12-27 17:37:07 +0100

    Updated to plugin api 0.8.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,36 @@
 #![feature(register_tool)]
 #![register_tool(tarpaulin)]
+#![allow(clippy::map_entry)]
 
 #[macro_use]
 extern crate query_interface;
 
 use std::sync::Arc;
 
-use crate::di::{profiles, Container, Provider};
 use inexor_rgf_core_di as di;
 use inexor_rgf_core_plugins as plugins;
 use log::error;
 
+use crate::di::profiles;
+use crate::di::Container;
+use crate::di::Provider;
 use crate::plugin::GraphQlClientPlugin;
-use crate::plugins::{Plugin, PluginError};
+use crate::plugins::Plugin;
+use crate::plugins::PluginDependency;
+use crate::plugins::PluginLoadingError;
 
 pub mod plugin;
 pub mod provider;
+
+pub static PLUGIN_NAME: &str = env!("CARGO_PKG_NAME");
+pub static PLUGIN_DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
+pub static PLUGIN_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 pub fn get<T>() -> Container<T> {
     Container::<T>::new()
 }
 
-pub fn construct_plugin() -> Result<Arc<dyn Plugin>, PluginError> {
+pub fn construct_plugin() -> Result<Arc<dyn Plugin>, PluginLoadingError> {
     let mut container = get::<profiles::Default>();
     let container = &mut container;
     let plugin = Provider::<dyn GraphQlClientPlugin>::create(container);
@@ -29,20 +38,24 @@ pub fn construct_plugin() -> Result<Arc<dyn Plugin>, PluginError> {
     let plugin: Result<Arc<dyn Plugin>, _> = <dyn query_interface::Object>::query_arc(plugin);
     if plugin.is_err() {
         error!("Failed to construct plugin");
-        return Err(PluginError::PluginCreationError);
+        return Err(PluginLoadingError::PluginContainerInitializationError);
     }
     Ok(plugin.unwrap())
 }
 
-plugins::export_plugin!(register);
+plugins::export_plugin!(register, get_dependencies, PLUGIN_NAME, PLUGIN_DESCRIPTION, PLUGIN_VERSION);
 
 #[allow(improper_ctypes_definitions)]
 extern "C" fn register(registrar: &mut dyn plugins::PluginRegistrar) {
-    const PKG_NAME: &str = env!("CARGO_PKG_NAME");
     if let Err(error) = log4rs::init_file("config/logging.toml", Default::default()) {
-        println!("Failed to configure logger in {}: {}", PKG_NAME, error);
+        println!("Failed to configure logger in {}: {}", PLUGIN_NAME, error);
     }
     if let Ok(plugin) = construct_plugin() {
-        registrar.register_plugin(PKG_NAME, Box::new(plugin));
+        registrar.register_plugin(Box::new(plugin));
     }
+}
+
+#[allow(improper_ctypes_definitions)]
+extern "C" fn get_dependencies() -> Vec<PluginDependency> {
+    vec![]
 }
--- src/plugin.rs
+++ src/plugin.rs
@@ -2,25 +2,20 @@ use std::sync::Arc;
 use std::sync::RwLock;
 
 use async_trait::async_trait;
-use log::debug;
+use log::info;
 
 use crate::di::*;
-use crate::plugins::plugin::PluginMetadata;
 use crate::plugins::plugin_context::PluginContext;
-use crate::plugins::ComponentBehaviourProvider;
-use crate::plugins::ComponentProvider;
-use crate::plugins::EntityBehaviourProvider;
-use crate::plugins::EntityTypeProvider;
-use crate::plugins::FlowProvider;
+use crate::plugins::web_resource_provider;
 use crate::plugins::Plugin;
-use crate::plugins::PluginError;
-use crate::plugins::RelationBehaviourProvider;
-use crate::plugins::RelationTypeProvider;
+use crate::plugins::PluginContextInitializationError;
 use crate::plugins::WebResourceProvider;
+use crate::plugins::WebResourceProviderError;
+use crate::plugins::{PluginActivationError, PluginContextDeinitializationError};
 use crate::provider::GraphQlClientWebResourceProviderImpl;
 
 #[wrapper]
-pub struct PluginContextContainer(RwLock<Option<std::sync::Arc<dyn PluginContext>>>);
+pub struct PluginContextContainer(RwLock<Option<Arc<dyn PluginContext>>>);
 
 #[provides]
 fn create_empty_plugin_context_container() -> PluginContextContainer {
@@ -44,73 +39,24 @@ interfaces!(GraphQlClientPluginImpl: dyn Plugin);
 impl GraphQlClientPlugin for GraphQlClientPluginImpl {}
 
 impl Plugin for GraphQlClientPluginImpl {
-    fn metadata(&self) -> Result<PluginMetadata, PluginError> {
-        Ok(PluginMetadata {
-            name: env!("CARGO_PKG_NAME").into(),
-            description: env!("CARGO_PKG_DESCRIPTION").into(),
-            version: env!("CARGO_PKG_VERSION").into(),
-        })
-    }
-
-    fn init(&self) -> Result<(), PluginError> {
-        debug!("GraphQlClientPluginModuleImpl::init()");
+    fn activate(&self) -> Result<(), PluginActivationError> {
+        let base_path = self.web_resource_provider.get_base_path().clone();
+        info!("\n    http://localhost:31415/{base_path}/graph\n    http://localhost:31415/{base_path}/dynamic-graph");
         Ok(())
     }
 
-    fn post_init(&self) -> Result<(), PluginError> {
-        debug!("GraphQlClientPluginModuleImpl::post_init()");
-        Ok(())
-    }
-
-    fn pre_shutdown(&self) -> Result<(), PluginError> {
-        debug!("GraphQlClientPluginModuleImpl::pre_shutdown()");
-        Ok(())
-    }
-
-    fn shutdown(&self) -> Result<(), PluginError> {
-        debug!("GraphQlClientPluginModuleImpl::shutdown()");
-        Ok(())
-    }
-
-    fn set_context(&self, context: Arc<dyn PluginContext>) -> Result<(), PluginError> {
+    fn set_context(&self, context: Arc<dyn PluginContext>) -> Result<(), PluginContextInitializationError> {
         self.context.0.write().unwrap().replace(context);
         Ok(())
     }
 
-    fn get_component_provider(&self) -> Result<Arc<dyn ComponentProvider>, PluginError> {
-        Err(PluginError::NoComponentProvider)
+    fn remove_context(&self) -> Result<(), PluginContextDeinitializationError> {
+        let mut writer = self.context.0.write().unwrap();
+        *writer = None;
+        Ok(())
     }
 
-    fn get_entity_type_provider(&self) -> Result<Arc<dyn EntityTypeProvider>, PluginError> {
-        Err(PluginError::NoEntityTypeProvider)
-    }
-
-    fn get_relation_type_provider(&self) -> Result<Arc<dyn RelationTypeProvider>, PluginError> {
-        Err(PluginError::NoRelationTypeProvider)
-    }
-
-    fn get_component_behaviour_provider(&self) -> Result<Arc<dyn ComponentBehaviourProvider>, PluginError> {
-        Err(PluginError::NoComponentBehaviourProvider)
-    }
-
-    fn get_entity_behaviour_provider(&self) -> Result<Arc<dyn EntityBehaviourProvider>, PluginError> {
-        Err(PluginError::NoEntityBehaviourProvider)
-    }
-
-    fn get_relation_behaviour_provider(&self) -> Result<Arc<dyn RelationBehaviourProvider>, PluginError> {
-        Err(PluginError::NoRelationBehaviourProvider)
-    }
-
-    fn get_flow_provider(&self) -> Result<Arc<dyn FlowProvider>, PluginError> {
-        Err(PluginError::NoFlowProvider)
-    }
-
-    fn get_web_resource_provider(&self) -> Result<Arc<dyn WebResourceProvider>, PluginError> {
-        let web_resource_provider = self.web_resource_provider.clone();
-        let web_resource_provider: Result<Arc<dyn WebResourceProvider>, _> = <dyn query_interface::Object>::query_arc(web_resource_provider);
-        if web_resource_provider.is_err() {
-            return Err(PluginError::NoWebResourceProvider);
-        }
-        Ok(web_resource_provider.unwrap())
+    fn get_web_resource_provider(&self) -> Result<Option<Arc<dyn WebResourceProvider>>, WebResourceProviderError> {
+        web_resource_provider!(self.web_resource_provider)
     }
 }
