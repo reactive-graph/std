74649fa04cc210485cfe7cc04876bbfc652096e5
Andreas Schaeffer <andreas@schaeffernet.de>
2022-12-27 17:11:04 +0100

    Extracted model into a separate crate (inexor-rgf-model-numeric)

---
+++ crates/model/src/component/numeric_gate.rs
@@ -0,0 +1,13 @@
+use crate::model::component_ty;
+use crate::model::entity_model;
+use crate::model::properties;
+use crate::model::PropertyInstanceGetter;
+use crate::model::PropertyInstanceSetter;
+use crate::NAMESPACE_NUMERIC;
+
+properties!(NumericGateProperties, (LHS, "lhs", 0.0), (RHS, "rhs", 0.0), (RESULT, "result", 0.0));
+
+component_ty!(COMPONENT_NUMERIC_GATE, NAMESPACE_NUMERIC, COMPONENT_NAME_NUMERIC_GATE, "numeric_gate");
+
+entity_model!(NumericGateF64, get result f64, set lhs f64, set rhs f64);
+entity_model!(NumericGateI64, get result i64, set lhs i64, set rhs i64);
