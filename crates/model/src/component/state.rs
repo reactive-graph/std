6c907b299926882dba393434c905f0adbca5ed52
Andreas Schaeffer <andreas@schaeffernet.de>
2022-12-27 22:45:55 +0100

    Generate semantic structs

--- crates/model/src/component/state.rs
+++ crates/model/src/component/state.rs
@@ -1,7 +1,17 @@
 use crate::model::component_ty;
+use crate::model::entity_model;
 use crate::model::properties;
+use crate::model::PropertyInstanceGetter;
+use crate::model::PropertyInstanceSetter;
 use crate::NAMESPACE_STATE;
 
 properties!(StateProperties, (STATE, "state", 0), (SET_STATE, "set_state", 0));
 
 component_ty!(COMPONENT_STATE, NAMESPACE_STATE, COMPONENT_NAME_STATE, "state");
+
+entity_model!(State, get value value, get state value, set set_state value);
+entity_model!(StateBool, get value bool, get state bool, set set_state bool);
+entity_model!(StateF64, get value f64, get state f64, set set_state f64);
+entity_model!(StateString, get value string, get state string, set set_state string);
+entity_model!(StateArray, get value array, get state array, set set_state array);
+entity_model!(StateObject, get value object, get state object, set set_state object);
--- crates/model/src/component/value.rs
+++ crates/model/src/component/value.rs
@@ -1,7 +1,16 @@
 use crate::model::component_ty;
+use crate::model::entity_model;
 use crate::model::properties;
+use crate::model::PropertyInstanceGetter;
 use crate::NAMESPACE_VALUE;
 
 properties!(ValueProperties, (VALUE, "value", ""));
 
 component_ty!(COMPONENT_VALUE, NAMESPACE_VALUE, COMPONENT_NAME_VALUE, "value");
+
+entity_model!(Value, get value value);
+entity_model!(ValueBool, get value bool);
+entity_model!(ValueF64, get value f64);
+entity_model!(ValueString, get value string);
+entity_model!(ValueArray, get value array);
+entity_model!(ValueObject, get value object);
