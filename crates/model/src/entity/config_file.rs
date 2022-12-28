d0c223cf06d1bff6f3d0ed4f5cfa43bab047b41c
Andreas Schaeffer <andreas@schaeffernet.de>
2022-12-28 21:57:43 +0100

    Extracted model into a separate crate

---
+++ crates/model/src/entity/config_file.rs
@@ -0,0 +1,16 @@
+use crate::model::entity_model;
+use crate::model::entity_ty;
+use crate::model::PropertyInstanceGetter;
+use crate::model::PropertyInstanceSetter;
+use crate::NAMESPACE_CONFIG;
+
+// All properties are defined in the component(s)
+
+entity_ty!(ENTITY_TYPE_CONFIG_FILE, NAMESPACE_CONFIG, ENTITY_TYPE_NAME_CONFIG_FILE, "config_file");
+
+entity_model!(
+    ConfigFile,
+    get result value,
+    set trigger bool,
+    set filename string
+);
