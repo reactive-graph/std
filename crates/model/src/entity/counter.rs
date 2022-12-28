4ea821b3faf545ae760bcd6adbcbce7a3a414f43
Andreas Schaeffer <andreas@schaeffernet.de>
2022-12-28 14:26:31 +0100

    Generate entity struct for counter

--- crates/model/src/entity/counter.rs
+++ crates/model/src/entity/counter.rs
@@ -1,6 +1,9 @@
 use crate::model::behaviour_ty;
 use crate::model::entity_behaviour_ty;
+use crate::model::entity_model;
 use crate::model::entity_ty;
+use crate::model::PropertyInstanceGetter;
+use crate::model::PropertyInstanceSetter;
 use crate::NAMESPACE_ARITHMETIC_U64;
 
 // All properties are defined in the component(s)
@@ -8,3 +11,9 @@ use crate::NAMESPACE_ARITHMETIC_U64;
 entity_ty!(ENTITY_TYPE_COUNTER, NAMESPACE_ARITHMETIC_U64, ENTITY_TYPE_NAME_COUNTER, "counter");
 behaviour_ty!(BEHAVIOUR_COUNTER, NAMESPACE_ARITHMETIC_U64, BEHAVIOUR_NAME_COUNTER, "counter");
 entity_behaviour_ty!(ENTITY_BEHAVIOUR_COUNTER, ENTITY_TYPE_COUNTER, BEHAVIOUR_COUNTER);
+
+entity_model!(
+    Counter,
+    get result u64,
+    set trigger bool,
+);
