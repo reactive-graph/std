129c940af78e590d4d9d5229fb3fcaabe7290d9e
Andreas Schaeffer <andreas@schaeffernet.de>
2022-12-30 17:32:21 +0100

    Added more unit tests

--- crates/plugin/src/tests/mod.rs
+++ crates/plugin/src/tests/mod.rs
@@ -1,3 +1,7 @@
 mod component_provider_test;
 mod rx_camel_case_test;
+mod rx_char_count_test;
+mod rx_concat_test;
+mod rx_contains_test;
+mod rx_is_lowercase_test;
 mod rx_pascal_case_test;
---
+++ crates/plugin/src/tests/rx_contains_test.rs
@@ -0,0 +1,51 @@
+use serde_json::json;
+
+use crate::behaviour::entity::string_comparison::StringComparisonFactory;
+use crate::behaviour::entity::string_comparison::STRING_COMPARISONS;
+use crate::builder::ReactiveEntityInstanceBuilder;
+use crate::model::BehaviourTypeId;
+use crate::model::EntityTypeId;
+use crate::model_string::StringComparison;
+use crate::model_string::StringComparisonProperties::LHS;
+use crate::model_string::StringComparisonProperties::RESULT;
+use crate::model_string::StringComparisonProperties::RHS;
+use crate::model_string::COMPONENT_STRING_COMPARISON;
+use crate::model_string::NAMESPACE_STRING;
+use crate::reactive::BehaviourFactory;
+use crate::reactive::BehaviourState;
+
+#[test]
+fn rx_contains_test() {
+    let behaviour_ty = BehaviourTypeId::new_from_type(NAMESPACE_STRING, "contains");
+    let entity_ty = EntityTypeId::new_from_type(NAMESPACE_STRING, "contains");
+
+    let reactive_instance = ReactiveEntityInstanceBuilder::new(entity_ty)
+        .property(LHS, json!(""))
+        .property(RHS, json!(""))
+        .property(RESULT, json!(false))
+        .component(COMPONENT_STRING_COMPARISON.clone())
+        .build();
+    let contains = StringComparison::from(reactive_instance.clone());
+
+    // No behaviour -> no change
+    contains.lhs("test");
+    contains.rhs("es");
+    assert!(!contains.result().unwrap());
+
+    // With behaviour
+    {
+        let f = STRING_COMPARISONS.get(&behaviour_ty).cloned().expect("Failed to find behaviour function");
+        let factory = StringComparisonFactory::new(behaviour_ty, f);
+        let behaviour = factory.create(reactive_instance.clone()).expect("Failed to create behaviour");
+        assert_eq!(BehaviourState::Connected, behaviour.get_state());
+
+        contains.lhs("test");
+        contains.rhs("es");
+        assert!(contains.result().unwrap());
+        contains.lhs("tet");
+        contains.rhs("es");
+        assert!(!contains.result().unwrap());
+        contains.rhs("et");
+        assert!(contains.result().unwrap());
+    }
+}
