47572c940ecec6a0f4df065e0e093c20ae26541e
Andreas Schaeffer <andreas@schaeffernet.de>
2022-12-30 02:11:22 +0100

    Cleanup

--- crates/plugin/src/behaviour/entity/gate/mod.rs
+++ crates/plugin/src/behaviour/entity/gate/mod.rs
@@ -1,7 +1,5 @@
-// pub use comparison_gate::ComparisonGate;
 pub use function::ComparisonGateFunction;
 pub use function::COMPARISON_GATES;
 
-// pub mod comparison_gate;
 pub mod behaviour;
 pub mod function;
