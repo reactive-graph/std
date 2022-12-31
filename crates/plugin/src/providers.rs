23ea2686173ade9384def4f42cfc06c8a4e96dc2
Andreas Schaeffer <andreas@schaeffernet.de>
2022-12-31 03:14:18 +0100

    Removed component provider impl

--- crates/plugin/src/providers.rs
+++ crates/plugin/src/providers.rs
@@ -1,2 +1,1 @@
-crate::plugins::component_provider_impl!(SystemEnvironment, "../../assets/types/components");
 crate::plugins::entity_type_provider_impl!(SystemEnvironment, "../../assets/types/entities");
