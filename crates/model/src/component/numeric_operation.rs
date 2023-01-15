5a79f9a019543de0ea038281fc8b596dd831469e
Andreas Schaeffer <andreas@schaeffernet.de>
2023-01-15 17:30:03 +0100

    Cleanup

--- crates/model/src/component/numeric_operation.rs
+++ crates/model/src/component/numeric_operation.rs
@@ -1,8 +1,6 @@
 use crate::model::component_ty;
 use crate::model::entity_model;
 use crate::model::properties;
-use crate::model::PropertyInstanceGetter;
-use crate::model::PropertyInstanceSetter;
 use crate::NAMESPACE_NUMERIC;
 
 properties!(NumericOperationProperties, (LHS, "lhs", 0.0), (RESULT, "result", 0.0));
