672d5340a2db9d79f3acab2d0d31fbf7de530f77
Andreas Schaeffer <andreas@schaeffernet.de>
2023-01-16 00:28:37 +0100

    Use Named from model_base

--- plugins/inexor-rgf-plugin-taxonomy/crates/model/src/entity/category.rs
+++ plugins/inexor-rgf-plugin-taxonomy/crates/model/src/entity/category.rs
@@ -1,5 +1,7 @@
 use crate::model::entity_model;
 use crate::model::entity_ty;
+use crate::model_base::Describable;
+use crate::model_base::Named;
 use crate::NAMESPACE_TAXONOMY;
 
 entity_ty!(ENTITY_TYPE_CATEGORY, NAMESPACE_TAXONOMY, ENTITY_TYPE_NAME_CATEGORY, "category");
@@ -9,5 +11,5 @@ entity_model!(
     get name string,
     get description string
 );
-// impl Named for Category {}
-// impl Describable for Category {}
+impl Named for Category {}
+impl Describable for Category {}
--- plugins/inexor-rgf-plugin-taxonomy/crates/model/src/lib.rs
+++ plugins/inexor-rgf-plugin-taxonomy/crates/model/src/lib.rs
@@ -7,5 +7,6 @@ pub mod entity;
 pub mod relation;
 
 use inexor_rgf_core_model as model;
+use inexor_rgf_model_base as model_base;
 
 pub const NAMESPACE_TAXONOMY: &str = "taxonomy";
