1d80ad9008778d3dcb095b09db311f89b540f84d
Andreas Schaeffer <andreas@schaeffernet.de>
2023-05-06 15:38:04 +0200

    GitHub Actions: Allow to override the web resource root via environment variable (try 2)

--- plugins/graphql-client/build.rs
+++ plugins/graphql-client/build.rs
@@ -6,8 +6,15 @@ use std::path::Path;
 fn main() -> Result<()> {
     let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     println!("CARGO_MANIFEST_DIR={}", dir);
-    let web_resource_root_dir = env::var("WEB_RESOURCE_DIR").unwrap_or(dir);
-    let web_resource_root_path = Path::new(&web_resource_root_dir).join("web/node_modules/yarn/bin/yarn");
+    let web_resource_root_path = match env::var("WORKSPACE_ROOT_MANIFEST_DIR") {
+        Ok(workspace_root_manifest_dir) => Path::new(&workspace_root_manifest_dir)
+            .join("plugins")
+            .join(env::var("CARGO_PKG_NAME").unwrap())
+            .join("web/node_modules/yarn/bin/yarn")
+            .canonicalize()
+            .unwrap(),
+        Err(_) => Path::new(&dir).join("web/node_modules/yarn/bin/yarn"),
+    };
     println!("PATH={}", web_resource_root_path.to_str().unwrap());
     let result = NpmBuild::new("web")
         .executable(web_resource_root_path.to_str().unwrap())
