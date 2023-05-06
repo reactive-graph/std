dc86e41805c390ce234a4ad99a0d15547e338d1c
Andreas Schaeffer <andreas@schaeffernet.de>
2023-05-06 16:45:07 +0200

    GitHub Actions: Allow to override the web resource root via environment variable (try 5)

--- plugins/graphql-schema-visualization/build.rs
+++ plugins/graphql-schema-visualization/build.rs
@@ -4,16 +4,21 @@ use std::io::Result;
 use std::path::Path;
 
 fn main() -> Result<()> {
-    let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    println!("CARGO_MANIFEST_DIR={}", dir);
+    let cargo_manifest_dir = env::var("CARGO_MANIFEST_DIR").expect("Missing env var CARGO_MANIFEST_DIR");
+    println!("CARGO_MANIFEST_DIR={}", cargo_manifest_dir);
     let web_resource_root_path = match env::var("WORKSPACE_ROOT_MANIFEST_DIR") {
-        Ok(workspace_root_manifest_dir) => Path::new(&workspace_root_manifest_dir)
-            .join("plugins")
-            .join(env::var("CARGO_PKG_NAME").unwrap())
-            .join("web/node_modules/yarn/bin/yarn")
-            .canonicalize()
-            .unwrap(),
-        Err(_) => Path::new(&dir).join("web/node_modules/yarn/bin/yarn"),
+        Ok(workspace_root_manifest_dir) => {
+            let cargo_pkg_name = env::var("CARGO_PKG_NAME").expect("Missing env var CARGO_PKG_NAME");
+            let yarn_location = Path::new(&workspace_root_manifest_dir)
+                .join("plugins")
+                .join(cargo_pkg_name)
+                .join("web/node_modules/yarn/bin/yarn");
+            let yarn_location_dir = yarn_location.to_str().expect("Failed to get yarn location dir");
+            yarn_location
+                .canonicalize()
+                .unwrap_or_else(|_| panic!("Failed to canonicalize {}", &yarn_location_dir))
+        }
+        Err(_) => Path::new(&cargo_manifest_dir).join("web/node_modules/yarn/bin/yarn"),
     };
     println!("PATH={}", web_resource_root_path.to_str().unwrap());
     let result = NpmBuild::new("web")
