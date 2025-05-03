b65df56636ac25725807317b16774f33c0ee39e8
aschaeffer <aschaeffer@users.noreply.github.com>
2025-05-03 16:18:30 +0000

    deploy: a9934a992e9a5bc931de708a813c9a4e71e98990

--- docs/reactive_graph_std_logical_model/entity/trigger/sidebar-items.js
+++ docs/reactive_graph_std_logical_model/entity/trigger/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["BEHAVIOUR_NAME_TRIGGER","ENTITY_TYPE_NAME_TRIGGER"],"enum":["TriggerProperties"],"static":["ENTITY_TYPE_TRIGGER"],"struct":["BEHAVIOUR_TRIGGER","ENTITY_BEHAVIOUR_TRIGGER","Trigger"]};
+window.SIDEBAR_ITEMS = {"constant":["BEHAVIOUR_NAME_TRIGGER","ENTITY_TYPE_NAME_TRIGGER"],"enum":["TriggerProperties"],"static":["BEHAVIOUR_TRIGGER","ENTITY_BEHAVIOUR_TRIGGER","ENTITY_TYPE_TRIGGER"],"struct":["Trigger"]};
